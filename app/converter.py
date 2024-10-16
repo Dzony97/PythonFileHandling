62d316a2d5e4cb7b0ec8a84d7c2628b64eaf7be1
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-16 20:50:53 +0200

    Add ConvertDataToListCars class responsible for converting data to a list of Car objects

--- app/converter.py
+++ app/converter.py
@@ -10,3 +10,8 @@ class Converter(ABC):
     @abstractmethod
     def convert(self, data: Data) -> list[Car]:
         pass
+
+
+class ConvertDataToListCars(Converter):
+    def convert(self, data: Data) -> list[Car]:
+        return [Car(**car) for car in data]
