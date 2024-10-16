bc9bf70679ffa6957f6ce7b20cb30bb0317e9d70
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-16 16:04:53 +0200

    Added CSVLoader class for converting data from CSV files

--- app/loader.py
+++ app/loader.py
@@ -8,3 +8,16 @@ class DataLoader[T](ABC):
     @abstractmethod
     def load(self, path: str) -> list[dict[str, T]]:
         pass
+
+
+class CSVLoader[T](DataLoader):
+    separator = ','
+
+    def load(self, path: str) -> list[dict[str, T]]:
+        with open(path) as csv_file:
+            reader = [{key: val for key, val in row.items()} for row in csv.DictReader(csv_file)]
+            for row in reader:
+                row['price'], row['mileage'], row['components'] = (int(row['price']), int(row['mileage']),
+                                                                   [component.strip() for component in
+                                                                    row['components'].split(self.separator)])
+            return reader
