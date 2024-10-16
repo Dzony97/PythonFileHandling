f1ec554ecabe398c46b1dc4b45638820ef7ec7b9
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-16 19:25:31 +0200

    Added CarNumberElementValidator class for validating car numerical values range

--- app/validator.py
+++ app/validator.py
@@ -10,3 +10,14 @@ class Validator[T](ABC):
     @abstractmethod
     def validate(self, data: Data) -> Data:
         pass
+
+
+@dataclass
+class CarNumberElementValidator(Validator):
+    min_range: float = 0
+    max_range: float = float('inf')
+    component: str = "price"
+
+    def validate(self, data: Data) -> Data:
+        return [car for car in data if isinstance(car[self.component], int)
+                and self.min_range < car[self.component] < self.max_range]
