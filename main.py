068a77cc7aa180215fce48483899aa9d674618ba
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-17 13:35:25 +0200

    Testing the functionality of validator, loader and converter

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from app.model import Car
 from app.loader import JsonDataLoader
 from app.converter import ConvertDataToListCars
+from app.validator import CarNumberElementValidator
 
 
 def main() -> None:
     json_data = JsonDataLoader()
     converter = ConvertDataToListCars()
-    print(converter.convert(json_data.load('data/data.json')))
+    validator = CarNumberElementValidator()
+    print(converter.convert(validator.validate(json_data.load('data/data.json'))))
 
 
 if __name__ == '__main__':
