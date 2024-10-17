fea433137036b9e5b34b5a3e19c9105a1c2bb122
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-17 14:00:29 +0200

    Add CarRepository class with loader, converter and validator logic

--- app/repository.py
+++ app/repository.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from abc import ABC, abstractmethod
+from typing import override
+
 from app.model import Car
 from app.loader import DataLoader
 from app.converter import Converter
@@ -16,3 +18,23 @@ class AbstractCarRepository(ABC):
     @abstractmethod
     def get_data(self) -> list[Car]:
         pass
+
+
+@dataclass
+class CarRepository(AbstractCarRepository):
+    loader: DataLoader
+    converter: Converter
+    validator: Validator
+
+    _cars: list[Car] = field(default_factory=list, init=False)
+
+    @override
+    def load_data(self, filename: str) -> list[Car]:
+        data = self.loader.load(filename)
+        self.validator.validate(data)
+        self._cars = self.converter.convert(data)
+        return self._cars
+
+    @override
+    def get_data(self) -> list[Car]:
+        return self._cars
