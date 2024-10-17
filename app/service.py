68d7d762bf6cd1635d4c84867b5bde30de33499b
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-17 14:08:06 +0200

    Add a helper method get_attribute in the CarService

--- app/service.py
+++ app/service.py
@@ -1,7 +1,14 @@
 from app.model import Car
 from dataclasses import dataclass
+from app.repository import AbstractCarRepository
 
 
 @dataclass
-class CarService:
-    pass
+class CarService[T]:
+    car_repository: AbstractCarRepository
+
+    def get_attributes(self, attribute: str) -> list[T]:
+        if attribute not in {'price', 'mileage', 'color', 'model', 'components', 'id'}:
+            raise ValueError('Invalid attribute')
+        cars = self.car_repository.get_data()
+        return [getattr(car, attribute) for car in cars]
