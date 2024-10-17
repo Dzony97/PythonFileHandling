a3ab0b03675d689232b2ea5dc544e2d439def519
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-17 17:30:35 +0200

    Add fake objects that will be used in tests

---
+++ tests/fake_objects.py
@@ -0,0 +1,5 @@
+from app.model import Car
+
+bmw_car = Car(model='BMW', price=260, color='BLACK', mileage=1400, components=['ABS', 'BLUETOOTH', 'ALLOY WHEELS'])
+audi_car = Car(model='AUDI', price=140, color='BLACK', mileage=1500, components=['ABS', 'ALLOY WHEELS'])
+mazda_car = Car(model='MAZDA', price=200, color='WHITE', mileage=2500, components=['AIR CONDITIONING', 'BLUETOOTH'])
